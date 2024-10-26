6ddb71eb00a7468a1c5a1a70909b88ca2f6ee79c
sherealu <sherealu@gmail.com>
2024-10-25 22:47:35 -0700

    Mostly completed code. Occasional line 19 error: list index out of range

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 #Purpose: To generate a random password
 #with numbers and letters
 
-import random 
+import random #module containing functions like C library
 
 alpha_string = "abcdefghijklmnopqrstuvwsyz"
 alpha_array = [x for x in alpha_string] #alpha_array = alpha_string.split() splits by spaces not chars
 
 #request user input for password length
-print("Enter password length below:")
-pass_length = input("enter password length:")
+pass_length = int(input("enter password length:"))
 
-n = 0
+password = [0]*pass_length
 
-for n in 26:
-    index = random.randrange(0,26)
+for n in range(pass_length): #loops through all index values of pass_length
+    index_check = random.randint(0,1) #1 = alphabet, 0 = number
+    
+    if index_check == 1:
+        index_alpha = random.randint(0,26)
+        password[n] = alpha_array[index_alpha]
 
+    else:
+        index_num = str(random.randint(0,10))
+        password[n] = index_num
 
-print("Your password is: " + password) #initiate password array
+pass_string = "".join(password)
+print("Your password is: " + pass_string)
